a0ef758a7c4f678b73cf19f1487a1d892a155e7e
Yythom <1078013818@qq.com>
2021-07-14 12:29:13 +0800

    fix

--- src/controllers/modules/map.js
+++ src/controllers/modules/map.js
@@ -1,13 +1,10 @@
 import Axios from 'axios';
-import api from '../../api/index';
 import Config from '../../utils/mapConfig';
-import request from '../../utils/request';
-import { combine } from '../../utils/utils';
 
 module.exports = {
     getLocationDetail: async (ctx, next) => {
         const data = ctx.request.body;
-        const url = `${Config.MAP_SERVER_URL}/ws/geocoder/v1/?address=${encodeURIComponent(data.address)}&get_poi=1&poi_options=radius=1000&key=${Config.LOCATION_KEY}`;
+        const url = `https://apis.map.qq.com/ws/geocoder/v1/?address=${encodeURIComponent(data.address)}&get_poi=1&poi_options=radius=1000&key=${Config.LOCATION_KEY}`;
         const res = await Axios.get(url);
         if (res.data.status === 0) {
             ctx.body = { code: 0, ...res.data };
@@ -15,5 +12,46 @@ module.exports = {
             ctx.body = { code: false, msg: '地址请求错误' };
         }
     },
+    getLocation: async (ctx, next) => {
+        const data = ctx.request.body;
+        const url = `https://apis.map.qq.com/ws/geocoder/v1/?location=${data.lat},${data.lng}&key=${Config.LOCATION_KEY}`;
+        const res = await Axios.get(url);
+        if (res.data.status === 0) {
+            ctx.body = { code: 0, ...res.data };
+        } else {
+            ctx.body = { code: false, msg: '地址请求错误' };
+        }
+    },
+    polyLine: async (ctx, next) => {
+        const data = ctx.request.body;
+        const url = `https://apis.map.qq.com/ws/direction/v1/bicycling/?from=${data.from}&to=${data.to}&key=${Config.LOCATION_KEY}`;
+        const res = await Axios.get(url);
+        if (res.data.status === 0) {
+            ctx.body = { code: 0, ...res.data };
+        } else {
+            ctx.body = { code: false, msg: '地址请求错误' };
+        }
+    },
+    locationKeyword: async (ctx, next) => {
+        const data = ctx.request.body;
+        const url = `https://apis.map.qq.com/ws/place/v1/suggestion/?keyword=${encodeURIComponent(data.keyword)}&page_index=1&page_size=20&key=${Config.LOCATION_KEY}`;
+        const res = await Axios.get(url);
+        if (res.data.status === 0) {
+            ctx.body = { code: 0, ...res.data };
+        } else {
+            ctx.body = { code: false, msg: '地址请求错误' };
+        }
+    },
+    searchAround: async (ctx, next) => {
+        const data = ctx.request.body;
+        const url = `https://apis.map.qq.com/ws/place/v1/search?boundary=nearby(${data.location},${data.distance || 1000})&keyword=${encodeURIComponent(data.keyword)}&page_size=20&page_index=1&key=${Config.LOCATION_KEY}`;
+        const res = await Axios.get(url);
+        if (res.data.status === 0) {
+            ctx.body = { code: 0, ...res.data };
+        } else {
+            ctx.body = { code: false, msg: '地址请求错误' };
+        }
+    },
+
 
 };
--- src/routers/modules/map.js
+++ src/routers/modules/map.js
@@ -1,4 +1,8 @@
 export default (router, middleware, controller) => {
     // 注册路由
     router.post('/getLocationDetail', controller.map.getLocationDetail);
+    router.post('/getLocation', controller.map.getLocation);
+    router.post('/polyLine', controller.map.polyLine);
+    router.post('/locationKeyword', controller.map.locationKeyword);
+    router.post('/searchAround', controller.map.searchAround);
 };
