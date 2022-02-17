06bcba0e85a009d67c7d488fa9cc02d57e7d3849
Yythom <1078013818@qq.com>
2022-02-17 14:11:53 +0800

    增加md5校验

--- src/controllers/modules/dcloud.js
+++ src/controllers/modules/dcloud.js
@@ -3,6 +3,7 @@ import Axios from 'axios';
 import { Apk } from 'node-apk';
 import path from 'path';
 import fs, { WriteStream } from 'fs-extra';
+import crypto from 'crypto';
 import {
     DEFAULT_SIZE, PUBLIC_DIR, TMP_DIR, http, getVersion,
 } from '../../utils/file/s_file';
@@ -26,19 +27,23 @@ module.exports = {
             const apk = new Apk(filePath);
             const appInfo = await apk.getManifestInfo();
             const { size } = fs.statSync(filePath);
+            const buffer = fs.readFileSync(filePath);
+            const hash = crypto.createHash('md5');
+            hash.update(buffer, 'utf8');
+            const md5 = hash.digest('hex');
             const initJson = {
                 Code: 0,
                 Msg: '',
                 UpdateStatus: 0,
-                VersionCode: 201,
+                VersionCode: appInfo.versionCode,
                 VersionName: appInfo.versionName,
                 UploadTime: ' ',
                 ModifyContent: '',
                 DownloadUrl: '',
                 ApkSize: (size / 1024),
-                ApkMd5: apkHashName.replaceAll('.apk').toLocaleUpperCase(),
+                ApkMd5: md5,
             };
-            const check = getVersion.splitVersion(`${version}`, appInfo.versionName, code, 201);
+            const check = getVersion.splitVersion(`${version}`, appInfo.versionName, code, appInfo.versionCode);
             if (check.isupgrad) {
                 initJson.ModifyContent = '\r\n1、您当前的App版本过低，为了给您提供更好的体验，请将您的App更新至最新版本';
                 initJson.UpdateStatus = 1;
